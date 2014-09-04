ff4e2ef3581b52fdf2d72808c1421b58240dea79
Gary Geisler <ggeisler@gmail.com>
2014-09-04 11:25:01 -0700

    Migrate Bootstrap column classes: replace BS 2 classes with BS 3 classes
    As a first step in the Bootstrap migration, change all 'span*' to 'col-md-*'.
    There will be much fine-tuning of columns for optimal responsiveness later.
    (That is, 'col-md-*' might not always be the best class to use; in some cases
    it might be 'col-sm-*' or 'col-xs-*', but that will be easier to determine
    on a page-by-page basis after the pages are halfway presentable.)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140827225903) do
+ActiveRecord::Schema.define(:version => 20140904172751) do
 
   create_table "annotations", :force => true do |t|
     t.integer  "user_id"
